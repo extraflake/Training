e8ce24545b714bb5edb6cc56c96ff15a8d27202a
Naufal Aji Wibowo <naufal.aji@mii.co.id>
2021-03-01 11:39:13 +0700

    initial commit

---
+++ Training/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Training.Models;
+
+namespace Training.Controllers
+{
+    public class HomeController : Controller
+    {
+        MyContext MyContext = new MyContext();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public JsonResult Get(string id)
+        {
+            var result = MyContext.PartMasters.Find(id);
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public ActionResult Post()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Post(PartMaster partMaster)
+        {
+            MyContext.PartMasters.Add(partMaster);
+            var result = MyContext.SaveChanges();
+            if(result > 0)
+                return Json(result, JsonRequestBehavior.AllowGet);
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
